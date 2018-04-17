9656af605f2c38a94d4e5766d1acd97b9487c257
Hans Kammler <coolmacmaniac@gmail.com>
2018-04-18 02:45:41 +0530

    Improving imports
    Adding module import entries in the package init file.

--- web/__init__.py
+++ web/__init__.py
@@ -1,1 +1,7 @@
 # -*- coding: utf-8 -*-
+
+from .HttpMethod import HttpMethod
+from .HttpException import HttpException
+from .HttpRequest import HttpRequest
+from .HttpResponse import HttpResponse
+from .HttpRequestManager import HttpRequestManager
